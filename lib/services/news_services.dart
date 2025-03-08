5395709173f8744d71b8554b54798b1d5e4eaf2f
Abdul-Rahman Hamza <abdulrahmanhamza2205@gmail.com>
2025-03-08 14:49:32 +0200

    finshed the get news service method

---
+++ lib/services/news_services.dart
@@ -0,0 +1,27 @@
+import 'package:dio/dio.dart';
+import 'package:news_vibes/models/articale_model.dart';
+
+class NewsServices {
+  final Dio dio;
+
+  NewsServices(this.dio);
+
+  getNews() async {
+    Response response = await dio.get(
+        'https://newsapi.org/v2/top-headlines?apiKey=004371e682d8476aa44d9688f70e0f3c&country=us&category=general');
+
+    Map<String, dynamic> jsonData = response.data;
+
+    List<dynamic> articles = jsonData['articles'];
+
+    List<ArticaleModel> articlesList = [];
+
+    for (var article in articles) {
+      ArticaleModel articaleModel = ArticaleModel(
+        image: article['urlToImage'],
+        title: article['title'],
+        subTitle: article['description'],
+      );
+    }
+  }
+}
